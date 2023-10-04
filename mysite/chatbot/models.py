47ca00f8f66ea77a39c0dd3acf2c13986074745e
todayist88 <todayist88@gmail.com>
2023-10-04 16:59:15 +0900

    go

--- mysite/chatbot/models.py
+++ mysite/chatbot/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class message(models.Model):
-    massage_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Message(models.Model):
+    massage_text = models.CharField(max_length=300)
+    sent_at = models.DateTimeField('date published', auto_now_add=True)
+    is_bot = models.BooleanField(default=False)
     def __str__(self)
         return self.massage_text
+    
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self)
+        return self.name
--- mysite/chatbot/urls.py
+++ mysite/chatbot/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns= {
     path("", views.index, name="index"),
+    path("chatbot/", views.index, name="index"),
+    path("admin/", views.index, name="index"),
+
 }
