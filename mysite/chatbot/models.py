50dc1ea69ceb587eb78cd5dfc329f955ea2d1874
todayist88 <todayist88@gmail.com>
2023-10-04 14:36:13 +0900

    go

--- mysite/chatbot/models.py
+++ mysite/chatbot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class message(models.Model):
+    massage_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self)
+        return self.massage_text
